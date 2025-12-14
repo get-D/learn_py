afbd84498497b9f0588ed33fa9cffdf02c2f6842
get-D <divyatwa5@gmail.com>
2025-12-15 01:09:27 +0530

    eligibility for voting

---
+++ practiceSet2/eligiblity.py
@@ -0,0 +1,8 @@
+# Create a program that checks if a person is eligible to vote (age >= 18).
+
+age = int(input("Enter your age: "))
+
+if age>=18 :
+    print("you are eligible to vote.")
+else:
+    print("you are not eligible to vote.")
