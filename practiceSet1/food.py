bec4a68496eb82bef5a042ac34e835819ca37bf1
get-D <divyatwa5@gmail.com>
2025-12-14 17:53:24 +0530

    taking user input

---
+++ practiceSet1/food.py
@@ -0,0 +1,8 @@
+# Write a program that:
+# 1.Asks the user for their favorite food. 
+# 2. Prints:
+# Wow! I also like <food>.
+
+food = input("what is your favorite food? ")
+
+print("Wow! I also like",food)
