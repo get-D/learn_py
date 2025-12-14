a30b91f179f50bbe0821a510988d30b50e4206cc
get-D <divyatwa5@gmail.com>
2025-12-15 01:15:04 +0530

    checking if a number is even or odd.

---
+++ practiceSet2/evenOdd.py
@@ -0,0 +1,10 @@
+# Write a program that takes a number from the user and prints “Even” if it is
+# even, otherwise “Odd”.
+
+
+num = int(input("Enter any number: "))
+
+if (num%2 == 0):
+    print("number is even")
+else:
+    print("number is odd")
