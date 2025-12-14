50a23dcacf470e9551df1c57ee7cac6888f9ef5c
get-D <divyatwa5@gmail.com>
2025-12-14 17:25:33 +0530

    variables and data types

--- practiceSet1/poem.py
+++ practiceSet1/poem.py
@@ -1,1 +1,5 @@
+# Write a program that prints the following poem using a single 
+# Twinkle, twinkle, little star,
+# How I wonder what you are!?
+
 print("Twinkle, twinkle, little star,\nHow I wonder what you are!")
---
+++ practiceSet1/vardt.py
@@ -0,0 +1,9 @@
+# Create variables to store: - Your name (string)- Your age (integer)- Your height in meters (float)- A boolean value representing whether you are a student
+# Print all of them in one line.
+
+name = "Divyatwa"
+age = 20
+height = 9.9
+isStudent = True
+
+print("Name:",name, "Age:",age, "Height:", height,"Student:",isStudent)
