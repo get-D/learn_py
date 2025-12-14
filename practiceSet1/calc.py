7ad05bda9cda5c4f2449e0adc4dec3900ee91ce3
get-D <divyatwa5@gmail.com>
2025-12-14 18:02:10 +0530

    simple calculator

---
+++ practiceSet1/calc.py
@@ -0,0 +1,11 @@
+# Write a program that:
+# Takes two numbers as input from the user.
+# Prints their sum, difference, product, and quotient
+
+x = int(input("enter a number: "))
+y = int(input("enter another number: "))
+
+print("Sum is:", x+y)
+print("difference is:", x-y)
+print("product is:", x*y)
+print("quotient is:", x/y)
