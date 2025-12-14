99c0a5051086f572f25c7caa201a597b49206c01
get-D <divyatwa5@gmail.com>
2025-12-14 18:16:02 +0530

    opetator challenge

---
+++ practiceSet1/operator.py
@@ -0,0 +1,9 @@
+# Write a program that:
+# 1. Takes an integer as input from the user.
+# 2. Prints the square and cube of that number.
+
+x = int(input("Enter a number: "))
+
+print("Square: ", pow(x, 2))
+print("Cube: ", pow(x, 3))
+
