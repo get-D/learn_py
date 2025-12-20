6191c0dd478a6f6496e973b7a506cfb8a30319d6
get-D <divyatwa5@gmail.com>
2025-12-21 01:05:28 +0530

    printing characters using string slicing

---
+++ practiceSet3/pSlice.py
@@ -0,0 +1,10 @@
+# Given text = "Python Programming" , do the following: 
+# 1. Print the first 6 characters
+# 2. Print the last 6 characters 
+# 3. Print every second character from the string
+
+text = "Python Programming"
+
+print("first 6 characters:", text[ : 6])
+print("first 6 characters:", text[ -6: ])
+print("Every second character:", text[ : : 2])
