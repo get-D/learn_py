d9e8ac8737f3cac5a719c09fd4269411ddd6797c
get-D <divyatwa5@gmail.com>
2025-12-21 01:21:10 +0530

    using string methods

---
+++ practiceSet3/sfind.py
@@ -0,0 +1,11 @@
+# Take the string " i love python programming " and:
+# 1. Remove extra spaces from both ends
+# 2. Convert it to title case 
+# 3. Count how many times the string "o" appears
+
+
+str1 = " i love python programming "
+
+print("After removing extra space from both ends:", str1.strip())
+print("Title case:", str1.title())
+print("number of times the string \"o\" appears:", str1.count("o"))
