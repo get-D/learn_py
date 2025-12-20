e5d8d1befa120ce824dec9dca62192eb71bec88b
get-D <divyatwa5@gmail.com>
2025-12-21 01:38:13 +0530

    string formating using format method

---
+++ practiceSet3/formatMethod.py
@@ -0,0 +1,8 @@
+# Using format() , create a sentence:
+# "My name is John and I am 25 years old."
+# by passing "John" and 25 as variables.
+
+name = "John"
+age = 25
+
+print("My name is {} and I am {} years old.".format(name, age))
