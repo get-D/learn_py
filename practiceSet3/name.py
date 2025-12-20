afdb23f9bdedae23e4881f48f7623fcb4ffc2a4a
get-D <divyatwa5@gmail.com>
2025-12-21 00:50:55 +0530

    first, last char and length of a string

---
+++ practiceSet3/name.py
@@ -0,0 +1,10 @@
+# Create a string variable name with your full name. Print:
+# The first character
+# The last character
+# The length of the string
+
+name = "Divyatwa Kirnapure"
+
+print("first character:", name[0])
+print("last character:", name[-1])
+print("length of the string:", len(name))
