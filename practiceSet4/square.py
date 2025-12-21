5f01f491b6d825a02e5fa0b182694785e061a194
get-D <divyatwa5@gmail.com>
2025-12-22 02:35:21 +0530

    function that returns square of given number

---
+++ practiceSet4/square.py
@@ -0,0 +1,12 @@
+# Write a function square(num) that returns the square of a given number. Test it with different numbers.
+
+def square(num):
+    return num*num 
+
+s1 = square(5)
+s2 = square(7)
+s3 = square(9)
+
+print(s1)
+print(s2)
+print(s3)
