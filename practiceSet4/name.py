302d6519733560cf3eb3f24f89badae557e02749
get-D <divyatwa5@gmail.com>
2025-12-22 02:43:12 +0530

    function to concatinate 2 strings

---
+++ practiceSet4/name.py
@@ -0,0 +1,8 @@
+# Write a function full_name(first, last) that takes first name and last name as parameters and returns a single string in the format "First Last" .
+
+def full_name(first, last):
+    return f" {first} {last}"
+
+name = full_name("Divyatwa", "Kirnapure")
+
+print(name)
