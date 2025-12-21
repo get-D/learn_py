2fc14f601e53eb14ba75b157f225e474cd54ce4b
get-D <divyatwa5@gmail.com>
2025-12-22 03:32:48 +0530

    using math module

---
+++ practiceSet4/calMath.py
@@ -0,0 +1,9 @@
+# Import the math module and use it to:
+# Find the square root of 144
+# Calculate sin(90°) (hint: use math.radians() )
+
+import math
+
+print("Square root of 144 is", math.sqrt(144))
+
+print("sin(90) degrees is", math.sin(math.radians(90)))
