0358b2e087b48e44660688d8a79ec82084d23f47
get-D <divyatwa5@gmail.com>
2025-12-23 03:38:36 +0530

    basic list and its length

---
+++ fruits.py
@@ -0,0 +1,13 @@
+# Create a list 
+# fruits = ["apple", "banana", "cherry"] .
+# Print the first fruit. 
+# Replace "banana" with "orange" .
+# Print the length of the list.
+
+fruits = ["apple", "banana", "cherry"]
+
+print("first fruit:", fruits[0])
+
+fruits[1] = "orange"
+
+print("length:", len(fruits))
