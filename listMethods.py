779977e6b365eef23dfe71f2cdb8e48f074a2456
get-D <divyatwa5@gmail.com>
2025-12-23 03:59:09 +0530

    list methods

---
+++ listMethods.py
@@ -0,0 +1,17 @@
+ 
+# Start with 
+# numbers = [5, 2, 9, 1, 7] and do the following:
+# Sort the list in ascending order.
+# Append the number 10 to the list.
+# Remove the number 2 from the list.
+
+numbers = [5, 2, 9, 1, 7]
+
+numbers.sort()
+
+print("sorted in ascending order:", numbers)
+
+
+numbers.append(10)
+
+numbers.remove(2)
