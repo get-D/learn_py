e411cb1bad872a0de5ee1d04a4f02d0eb189e6a3
get-D <divyatwa5@gmail.com>
2025-12-23 03:43:48 +0530

    slicing in list

---
+++ numList.py
@@ -0,0 +1,8 @@
+# Create a list of numbers from 1 to 10 .
+# Print the first three numbers using slicing.
+# Print the last three numbers using slicing
+
+num = [1,2,3,4,5,6,7,8,9,10]
+
+print("first three numbers:", num[ : 3])
+print("last three numbers:", num[ -3: ])
