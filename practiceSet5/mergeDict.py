79f7515fe30553a435e5b962606a8cc8e42261e9
get-D <divyatwa5@gmail.com>
2025-12-24 13:01:32 +0530

    merging two dictionaries

---
+++ practiceSet5/mergeDict.py
@@ -0,0 +1,8 @@
+# Write a program that merges two dictionaries into one
+
+prodCost = {"chocolate": 110, "teady": 250, "earphones": 199, "coke": 20}
+call = {"div": 5458489875, "tanshiq": 9856682695, "gokul bhaiya": 9856843915}
+
+call.update(prodCost)
+
+print("updated dict:", call)
