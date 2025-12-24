38c9669b2abf03bd0eb4ab98a00965b16ce2ec0e
get-D <divyatwa5@gmail.com>
2025-12-24 11:35:38 +0530

    dictionary methods

---
+++ practiceSet5/dictMethods.py
@@ -0,0 +1,10 @@
+# Create a dictionary of three friends and their phone numbers. Use:
+# keys() to get all names
+# values() to get all numbers
+# items() to loop over key-value pairs and print them
+
+call = {"div": 5458489875, "tanshiq": 9856682695, "gokul bhaiya": 9856843915}
+
+print("Names:", call.keys())
+print("phone numbers:", call.values())
+print("maped phone numbers:", call.items())
