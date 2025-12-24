53bcf192974ed081a76f17bd3dcf9d95be84d5d4
get-D <divyatwa5@gmail.com>
2025-12-24 11:20:46 +0530

    set methods

---
+++ practiceSet5/setMethods.py
@@ -0,0 +1,14 @@
+# Create two sets:
+# a = {1, 2, 3}
+# b = {3, 4, 5}
+# Find their:
+# Union
+# Intersection
+# Difference (a - b)
+
+a = {1, 2, 3}
+b = {3, 4, 5}
+
+print("Union:",a.union(b))
+print("Intersection:",a.intersection(b))
+print("Difference:",a.difference(b))
