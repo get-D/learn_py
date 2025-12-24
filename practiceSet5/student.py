6c58f160dbcde5c049554ff140037f6bc81a0b52
get-D <divyatwa5@gmail.com>
2025-12-24 11:27:01 +0530

    dictionary example

---
+++ practiceSet5/student.py
@@ -0,0 +1,13 @@
+# Create a dictionary and:
+# student = {"name": "John", "age": 20, "grade": "A"}
+# Print the value of "name" .
+# Change "grade" to "A+" .
+# Add a new key "city" with value "Delhi" .
+
+student = {"name": "John", "age": 20, "grade": "A"}
+
+print("name:", student["name"])
+student["grade"] = "A+"
+student["city"] = "Delhi"
+
+print(student)
