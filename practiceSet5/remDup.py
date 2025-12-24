f0ccce858b7ab04573299aac9a9d55a12b43e950
get-D <divyatwa5@gmail.com>
2025-12-24 12:41:09 +0530

    removing duplicates from a list

---
+++ practiceSet5/remDup.py
@@ -0,0 +1,6 @@
+# Write a program that takes a list of numbers and removes all duplicates using a set.
+
+lst = [1,3,45,3,5,23,5,34,65,34,3,6]
+
+s = set(lst)
+print("after removing duplicates:", s)
