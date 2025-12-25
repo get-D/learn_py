4240046536f018e319186a96003f80ae0dedfcc5
get-D <divyatwa5@gmail.com>
2025-12-26 00:47:29 +0530

    class example

---
+++ practiceSet6/car.py
@@ -0,0 +1,10 @@
+# Create a class Car with a method drive() that prints "Car is moving" 
+# Create an object of Car and call drive() .
+
+class Car:
+    def drive(self):
+        print("Car is moving")
+
+c = Car()
+
+c.drive()
