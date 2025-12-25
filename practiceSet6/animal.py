003debecf686ae9cfc801afda8a41cfcc812dcfc
get-D <divyatwa5@gmail.com>
2025-12-26 01:07:12 +0530

    inheritance example

---
+++ practiceSet6/animal.py
@@ -0,0 +1,16 @@
+# Create a base class Animal with a method sound() that prints "Some sound" .
+# Create a derived class Dog that overrides sound() to print "Bark!" .
+# Create an object of Dog and call sound() 
+
+class Animal:
+    def sound(self):
+        print("Some sound")
+
+
+class Dog(Animal):
+    def sound (self):
+        print("Bark!")
+
+rusty = Dog()
+
+rusty.sound()
