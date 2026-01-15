e92b1c782b92f14b1b36ae436c84d8578a245b63
get-D <divyatwa5@gmail.com>
2026-01-15 19:06:05 +0530

    writing three lines

---
+++ practiceSet8/task.py
@@ -0,0 +1,7 @@
+# Write a program that writes three lines of text to a file tasks.txt .
+
+with open("task.txt", "w") as f:
+    f.write("line 1 \n")
+    f.write("line 2 \n")
+    f.write("line 3 \n")
+    
