1ce16753b994492872e5c6c4f03cbc682046d379
get-D <divyatwa5@gmail.com>
2026-01-16 09:41:16 +0530

    using os module

---
+++ practiceSet8/os.py
@@ -0,0 +1,11 @@
+#  Use the os module to:
+# 1. Print the current working directory
+# 2. List all files and folders in the current directory
+# 3. Create a new folder my_folder
+
+
+import os
+
+print(os.getcwd())
+print(os.listdir())
+os.mkdir("my_folder")
